1d34de2eec244a985f7926fed792735ddefe7ced
Cristiano Sellitto <cristiano.sellitto@mths.ca>
2022-11-28 16:19:24 +0000

    Removed surface area function

--- calculatingSpheres.cpp
+++ calculatingSpheres.cpp
@@ -2,22 +2,20 @@
 //
 // Created by Cristiano Sellitto
 // Created in November 2022
-// A program that calculates a sphere's area and volume
+// A program that calculates a sphere's volume
 
+#include <cmath>
 #include <iostream>
 #include <string>
-#include <cmath>
 
 float radiusNumber;
-float surfaceArea;
 float volume;
 
-int main() {
-    // Calculates a sphere's area and volume
+void volumeOfASphere() {
+    // Calculates a sphere's volume
 
-    std::cout << "This program calculates a sphere's surface area and volume";
+    std::cout << "This program calculates a sphere's surface volume.";
     std::cout << std::endl;
-    std::cout << "A = 4πr²" << std::endl;
     std::cout << "V = (4 ÷ 3) × πr³" << std::endl;
     std::cout << std::endl;
     try {
@@ -25,9 +23,7 @@ int main() {
         std::cout << "Enter the sphere's radius: ";
         std::cin >> radiusText;
         radiusNumber = stof(radiusText);
-        surfaceArea = 4 * M_PI * pow(radiusNumber, 2);
         volume = (4.0 / 3.0) * M_PI * pow(radiusNumber, 3);
-        std::cout << "\nThis sphere's surface area is " << surfaceArea << "cm²";
         std::cout << std::endl;
         std::cout << "This sphere's volume is " << volume << "cm³";
         std::cout << std::endl;
@@ -38,4 +34,10 @@ int main() {
 
     std::cout << std::endl;
     std::cout << "Done." << std::endl;
+}
+
+int main() {
+    // Calls a function
+
+    volumeOfASphere();
 }
